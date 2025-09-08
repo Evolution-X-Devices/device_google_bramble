67949b9943d86813483deb5992b453806379d0b2
Apelete Seketeli <apelete@seketeli.net>
2025-09-08 12:12:57 +0200

    bramble: Select apps based on WITH_GMS flag
    Selectively build with or without GAPPS and allow files to live in
    specified root path depending on WITH_GMS flag.

--- lineage_bramble.mk
+++ lineage_bramble.mk
@@ -27,6 +27,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=google/bramble/bramble:14/UP1A.231105.001.B2/11260668:user/release-keys \
     DeviceProduct=bramble
 
+ifeq ($(WITH_GMS),false)
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/priv-app/OmniStyle/OmniStyle.apk
+else
 TARGET_USES_MINI_GAPPS := true
 
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
@@ -36,5 +40,6 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/etc/permissions/privapp-permissions-google.xml \
     system/priv-app/DocumentsUIGoogle/DocumentsUIGoogle.apk \
     system/priv-app/TagGoogle/TagGoogle.apk
+endif
 
 $(call inherit-product, vendor/google/bramble/bramble-vendor.mk)
